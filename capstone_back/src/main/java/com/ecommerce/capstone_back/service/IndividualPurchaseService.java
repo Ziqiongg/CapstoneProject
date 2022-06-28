fc9495ebb1207b25cb716ff41b0f90042341a038
Joseph <Josephsut@gmail.com>
2022-06-28 10:39:17 +0100

    Settup Repos+Services

--- capstone_back/src/main/java/com/ecommerce/capstone_back/service/IndividualPurchaseService.java
+++ capstone_back/src/main/java/com/ecommerce/capstone_back/service/IndividualPurchaseService.java
@@ -1,4 +1,16 @@
 package com.ecommerce.capstone_back.service;
 
+import com.ecommerce.capstone_back.repository.IndividualPurchaseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class IndividualPurchaseService {
+
+    public final IndividualPurchaseRepository individualPurchaseRepository;
+
+    @Autowired
+    public IndividualPurchaseService(IndividualPurchaseRepository individualPurchaseRepository) {
+        this.individualPurchaseRepository = individualPurchaseRepository;
+    }
 }
--- capstone_back/src/main/java/com/ecommerce/capstone_back/service/ProductService.java
+++ capstone_back/src/main/java/com/ecommerce/capstone_back/service/ProductService.java
@@ -1,4 +1,15 @@
 package com.ecommerce.capstone_back.service;
 
+import com.ecommerce.capstone_back.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Autowired
 public class ProductService {
+
+    public final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
 }
