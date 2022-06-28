7be3efe92806aa967dc5e23e785ecddfc272e363
Joseph <Josephsut@gmail.com>
2022-06-28 14:12:05 +0100

    Added ENUMs

---
+++ capstone_back/src/main/java/com/ecommerce/capstone_back/Category.java
@@ -0,0 +1,13 @@
+package com.ecommerce.capstone_back;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public enum Category {
+    HATS,
+    BOATS,
+    OATS,
+    MOATS,
+    GOATS
+};
+
+
