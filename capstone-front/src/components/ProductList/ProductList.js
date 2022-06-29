be90e108f4cda1115d9964dfe005fd01044feaf8
Carol Li <97911234+Ziqiongg@users.noreply.github.com>
2022-06-29 15:05:06 +0100

    Carol front (#29)
    * delete useless files

---
+++ capstone-front/src/components/ProductList/ProductList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './ProductList.css';
+
+const ProductList = () => (
+  <div className="ProductList">
+    ProductList Component
+  </div>
+);
+
+ProductList.propTypes = {};
+
+ProductList.defaultProps = {};
+
+export default ProductList;
---
+++ capstone-front/src/components/Register/Register.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Register.css';
+
+const Register = () => (
+  <div className="Register">
+    Register Component
+  </div>
+);
+
+Register.propTypes = {};
+
+Register.defaultProps = {};
+
+export default Register;
