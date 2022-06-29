3052460a8e414719ba76c40dd0c8b631efcd9384
alh40 <alexander.leaver@me.com>
2022-06-29 15:25:42 +0100

    fixed patch request

--- capstone_back/src/main/java/com/ecommerce/capstone_back/controllers/AppUserController.java
+++ capstone_back/src/main/java/com/ecommerce/capstone_back/controllers/AppUserController.java
@@ -8,6 +8,7 @@ import com.ecommerce.capstone_back.service.AppUserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.annotation.ApplicationScope;
 
 import java.util.Optional;
 
@@ -15,6 +16,7 @@ import java.util.Optional;
 public class AppUserController {
 
     public final AppUserService appUserService;
+
 
     public AppUserController(AppUserService appUserService) {
         this.appUserService = appUserService;
@@ -47,22 +49,64 @@ public class AppUserController {
         return ResponseEntity.ok().body(appUser.getWallet());
    }
 
-   // Super update all things as needed
-    @PatchMapping("/users/{id}")
-    public ResponseEntity<AppUser> updateAppUser(@PathVariable Long id,
-                                                 @RequestParam Optional<Double> wallet,
-                                                 @RequestParam Optional<String> firstName,
-                                                 @RequestParam Optional<String> lastName,
-                                                 @RequestParam Optional<String> address) throws Exception {
+   // Super update all things as needed --- Does not work, keeping as extension later on
+//    @PatchMapping("/users/{id}")
+//    public ResponseEntity<AppUser> updateAppUser(@PathVariable Long id,
+//                                                 @RequestParam Optional<Double> wallet,
+//                                                 @RequestParam Optional<String> firstName,
+//                                                 @RequestParam Optional<String> lastName,
+//                                                 @RequestParam Optional<String> address) throws Exception {
+//        AppUser appUser = appUserService.getAppUserById(id);
+//        wallet.ifPresent(appUser::setWallet);
+//        firstName.ifPresent(appUser::setUserFirstName);
+//        lastName.ifPresent(appUser::setUserLastName);
+//        address.ifPresent(appUser::setUserAddress);
+//        return ResponseEntity.ok().body(appUser);
+//    }
+
+    @PatchMapping("/users/patch/firstName/{id}")
+    public ResponseEntity<AppUser> updateAppUserFirstName(@PathVariable Long id, @RequestBody String firstName) throws Exception{
         AppUser appUser = appUserService.getAppUserById(id);
-        wallet.ifPresent(appUser::setWallet);
-        firstName.ifPresent(appUser::setUserFirstName);
-        lastName.ifPresent(appUser::setUserLastName);
-        address.ifPresent(appUser::setUserAddress);
+        appUser.setUserFirstName(firstName);
+        appUserService.updateAppUser(appUser);
         return ResponseEntity.ok().body(appUser);
     }
 
-   @DeleteMapping("/users/{id}")
+    @PatchMapping("/users/patch/lastName/{id}")
+    public ResponseEntity<AppUser> updateAppUserLastName(@PathVariable Long id, @RequestBody String lastName) throws Exception{
+        AppUser appUser = appUserService.getAppUserById(id);
+        appUser.setUserLastName(lastName);
+        appUserService.updateAppUser(appUser);
+        return ResponseEntity.ok().body(appUser);
+    }
+
+    @PatchMapping("/users/patch/address/{id}")
+    public ResponseEntity<AppUser> updateAppUserAddress(@PathVariable Long id, @RequestBody String address) throws Exception{
+        AppUser appUser = appUserService.getAppUserById(id);
+        appUser.setUserAddress(address);
+        appUserService.updateAppUser(appUser);
+        return ResponseEntity.ok().body(appUser);
+    }
+
+    @PatchMapping("/users/patch/password/{id}")
+    public ResponseEntity<AppUser> updateAppUserPassword(@PathVariable Long id, @RequestBody String password) throws Exception{
+        AppUser appUser = appUserService.getAppUserById(id);
+        appUser.setUserPassword(password);
+        appUserService.updateAppUser(appUser);
+        return ResponseEntity.ok().body(appUser);
+    }
+
+    @PatchMapping("/users/patch/wallet/{id}")
+    public ResponseEntity<AppUser> updateAppUserWallet(@PathVariable Long id, @RequestBody double wallet) throws Exception{
+        AppUser appUser = appUserService.getAppUserById(id);
+        appUser.setWallet(wallet);
+        appUserService.updateAppUser(appUser);
+        return ResponseEntity.ok().body(appUser);
+    }
+
+
+
+    @DeleteMapping("/users/{id}")
    public void deleteAppUser(@PathVariable Long id) throws Exception {
        AppUser appUser = appUserService.getAppUserById(id);
        appUserService.deleteAppUser(appUser);
