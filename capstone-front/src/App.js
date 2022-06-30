5a3cb804652b10d7bce207aa3a682b8ea2ce41ef
Carol Li <97911234+Ziqiongg@users.noreply.github.com>
2022-06-30 09:55:08 +0100

    add route (#36)

--- capstone-front/src/App.js
+++ capstone-front/src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
+import ProductList from './components/ProductList/ProductList';
+import Landing from "./components/Landing/Landing";
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
+ 
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />}>
+          <Route path="productlist" element={<ProductList />} />
+          {/* <Route path="contact" element={<Contact />} /> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  
   );
 }
 
