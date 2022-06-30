c369480c635a77f646d53c6d01ca147dd67ae8bb
Peace Akib <99202770+pe-a-ce@users.noreply.github.com>
2022-06-30 10:10:32 +0100

    product images added

--- capstone-front/src/components/Landing/Landing.js
+++ capstone-front/src/components/Landing/Landing.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './Landing.css';
+import BigHat from'./ProductImages/BigBoiHat.jpg';
 
 const Landing = () => (
-  <div className="Landing">
-    Landing Component
+  <section>
+{/* first image grid */}
+   <div className="Landing">
+   Landing Component
+ </div>
+{/* image carousel  */}
+  
+  
+
+{/* images container */}
+
+<div class="product-images-carousel">
+    <div class="main-carousel">1 / 3</div>
+    <img src={BigHat} alt='pingu hat'></img>
+    <div class="text">Big Boi Hat</div>
   </div>
+
+
+
+
+{/* second image grid */}
+<div className="Landing">
+  Landing Component
+</div>
+  </section> 
 );
 
 Landing.propTypes = {};
