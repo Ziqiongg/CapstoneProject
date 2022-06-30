7bcd5af9039d9675262774e40d577429a0cf8d3c
alh40 <alexander.leaver@me.com>
2022-06-30 15:37:39 +0100

    linking pages

--- capstone-front/src/App.js
+++ capstone-front/src/App.js
@@ -11,7 +11,7 @@ import ProductList from './components/ProductList/ProductList';
 import Landing from "./components/Landing/Landing";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-
+import Register from "./components/Register/Register";
 function App() {
   return (
 
@@ -21,8 +21,8 @@ function App() {
       <Routes>
           <Route exact path="/" element={<Landing />} />
           <Route exact path="/productlist" element={<ProductList />} />
+          <Route path="/register" element={<Register />} />
           {/* <Route path="/login"  element={<LogInPage />}/>
-          <Route path="/register" element={<RegisterPage />} />
           <Route path="/settings" element={<SettingsPage />} />
           <Route path="/feedPage" element={<FeedPage />} /> */}
       </Routes>
