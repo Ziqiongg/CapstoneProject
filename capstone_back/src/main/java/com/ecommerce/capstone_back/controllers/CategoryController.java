3b338be0845e157c5365e9917998fbe317ab5c21
Michelle Tambi <michelletambi22@gmail.com>
2022-07-03 17:52:29 +0100

    added JWT dependencies to the pom.xml

--- capstone_back/src/main/java/com/ecommerce/capstone_back/controllers/CategoryController.java
+++ capstone_back/src/main/java/com/ecommerce/capstone_back/controllers/CategoryController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-@CrossOrigin(origins = "http://localhost:3000")
+//@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class CategoryController {
     @GetMapping("/categories")
