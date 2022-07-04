c0f4b718e42aa141b837e1cddd44429c06761932
Carol Li <97911234+Ziqiongg@users.noreply.github.com>
2022-07-04 10:11:41 +0100

    Carol front (#52)
    * product list
    
    * database update
    
    * add description - db
    
    * basic product list done
    
    * change product page button
    
    * nothing
    
    * handel click go to detail page
    
    * category endpoint
    
    * create different routers for categories
    
    * choose category and jump to the product list
    
    * try to make useeffect keep re rending
    
    * the re rendering
    
    * click category function done

--- capstone-front/src/App.js
+++ capstone-front/src/App.js
@@ -12,19 +12,47 @@ import Landing from "./components/Landing/Landing";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Register from "./components/Register/Register";
-import Login from  "./components/Login/Login"
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+
+
 function App() {
+
+    //get categories
+    const [category, setCategory] = useState([]);
+
+    useEffect(() =>{
+  
+      axios.get('http://localhost:8080/categories')
+        .then(response => {
+       
+          setCategory(response.data);
+    
+        })
+        .catch(error => {console.log(error)})
+      }, [])
+
   return (
 
     <Router>
       <div className = "main-wrapper">
-      <Header />
+      <Header categories= {category} />
      
       <Routes>
           <Route exact path="/" element={<Landing />} />
-          <Route exact path="/productlist" element={<ProductList />} />
+          <Route exact path="/productlist" element={<ProductList 
+          address = "http://localhost:8080/products" />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/login"  element={<Login />}/>
+
+          {category.map(item => {
+            let itemLower = item.toLowerCase();
+            return (  
+              <Route exact path={`/productcategory/${itemLower}`} element={<ProductList 
+          address = {`http://localhost:8080/products/category/${itemLower}`} />} /> 
+          )
+          })}
+
           {/* 
           <Route path="/settings" element={<SettingsPage />} />
           <Route path="/feedPage" element={<FeedPage />} /> */}
