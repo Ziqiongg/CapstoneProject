806e10b5f5d3e7386fdd67f013477f6f5552e369
Carol Li <ziqili33@gmail.com>
2022-07-06 16:12:00 +0100

    different status for logging in and out

--- capstone-front/src/App.js
+++ capstone-front/src/App.js
@@ -91,23 +91,9 @@ function App() {
           <Route path = "/ourmissionpage" element = {<AboutUs />}/>
           <Route path = "/ourpledge" element = {<OurPledge />}/>
           <Route path = "/conditions" element = {<TermsConditions />}/>
-          <Route path="/basket" element={<Basket />} />
           <Route path={`/searchname`} element = {<SearchBar />} />
           <Route path={`/login`} element = {<Login />} />
-          {/* <Route path="/settings" element={<SettingsPage />} />
-
-          <Route path="/basket" element={<Basket basketItem={basketItem} BuyBasket={PurchaseItems} Delete={DeleteFromBasket}/>} />
-          <Route path="/orders" element={<Order basketItem={[basketItem]} />} />
-          <Route path = "/ourmissionpage" element = {<AboutUs />}/>
-          <Route path = "/conditions" element = {<TermsConditions />}/>
-
-
-          <Route path={`/searchname`} element = {<SearchBar />} />
-
-
-          {/* 
-          <Route path="/settings" element={<SettingsPage />} />
-          <Route path="/feedPage" element={<FeedPage />} /> */}
+        
 
       </Routes>
 
